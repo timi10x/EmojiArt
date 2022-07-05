39fd82f28035d00d14f3c518ae13845fff94f170
timmyCoder <olatoyedan@gmail.com>
2022-07-06 00:35:11 +0100

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Daniel Olatoye on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
