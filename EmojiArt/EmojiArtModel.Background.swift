f27a087681ebb0470897296d1b55762a4e513a76
timmyCoder <olatoyedan@gmail.com>
2022-07-07 01:40:29 +0100

    initial commit

---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by Daniel Olatoye on 06/07/2022.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let uRL): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data):
+                return data
+            default: return nil
+            }
+        }
+    }
+}
